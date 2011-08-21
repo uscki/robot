965dbbd3d785a03ec1dbf14d334e9b60eded8614
Jetze Baumfalk <breakingmyshell+github@gmail.com>
2011-08-21 17:58:37 +0200

    MennoV1 is born!

--- nl/uscki/robot/mennoveen/MennoVeen.java
+++ nl/uscki/robot/mennoveen/MennoVeen.java
@@ -1,6 +1,6 @@
-package nl.uscki.robot.head;
+package nl.uscki.robot.mennoveen;
 
-public class main {
+public class MennoVeen {
 	public static void main(String [] args) {
 		System.out.println("How many roads must a man walk?");
 	}
