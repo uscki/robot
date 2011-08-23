00a253c38bd66e605048f80c269203d9fcdc9f6d
Jetze Baumfalk <breakingmyshell+github@gmail.com>
2011-08-23 11:16:58 +0200

    Mogelijkheid tot Private Messaging met MennoV1 op IRC toegevoegd.

--- src/mennov1/MennoV1Irc.java
+++ src/mennov1/MennoV1Irc.java
@@ -11,6 +11,24 @@ public class MennoV1Irc extends PircBot {
 		this.setName("MennoV1");
 	}
 	
+	
+	
+	@Override
+	protected void onPrivateMessage(String sender, String login,
+			String hostname, String message) {
+		// TODO Auto-generated method stub
+		super.onPrivateMessage(sender, login, hostname, message);
+		String[] outputs = MennoV1.getInstance().parseArguments(message, sender);
+		for(String s : outputs) {
+			if(null == s){
+				continue;
+			}
+			sendMessage(sender, s);
+		}
+	}
+
+
+
 	public void onMessage(String channel, String sender,
 			String login, String hostname, String message) {
 		String[] outputs = MennoV1.getInstance().parseArguments(message, sender);
