a37b30341c1acaff954da70504647b7619667a1c
Vincent <projects@vinnl.nl>
2011-08-23 12:33:44 +0200

    IRC-client is nu maar een bestand, en laat zien wat ie doet.

--- src/mennov1/IrcClient.java
+++ src/mennov1/IrcClient.java
@@ -2,16 +2,33 @@ package mennov1;
 
 import org.jibble.pircbot.*;
 
-public class IrcClient {
+public class IrcClient extends PircBot {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) throws Exception{
-		MennoV1Irc bot = new MennoV1Irc();
+		IrcClient bot = new IrcClient();
 		
 		bot.connect("irc.enterthegame.com");
 		bot.joinChannel("#incognito");
+	}
+	
+	public IrcClient() {
+		this.setName("MennoV1");
+	}
+
+	public void onMessage(String channel, String sender,
+			String login, String hostname, String message) {
+		System.out.println("Received message from " + sender + " in " + channel + ": " + message);
+		String[] outputs = MennoV1.getInstance().parseArguments(message, sender);
+		for(String s : outputs) {
+			if(null == s){
+				continue;
+			}
+			System.out.println("Sent message: " + s);
+			sendMessage(channel, s);
+		}
 	}
 
 }
