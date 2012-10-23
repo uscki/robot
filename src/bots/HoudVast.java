ad2dee1c41d5c26c271129186bc7320a0d10ad05
Rick <ikbenrick@gmail.com>
2012-10-23 16:33:08 +0300

    Update src/bots/HoudVast.java
    nu!

--- src/bots/HoudVast.java
+++ src/bots/HoudVast.java
@@ -57,7 +57,8 @@ public class HoudVast extends AnswerBot
                 msg += item;
                 items = item;
             }
-        }
-        return (msg + ".");
+
+            return (msg + ".");
+        } else{return void;}
     }
 }
