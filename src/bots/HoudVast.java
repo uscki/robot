bb4a0a9f7b003b2784b0acfbe4f1fb55924eae3a
Soulfix <breakingmyshell+github@gmail.com>
2012-10-23 16:45:02 +0300

    Update src/bots/HoudVast.java
    Useless bot is useless

--- src/bots/HoudVast.java
+++ src/bots/HoudVast.java
@@ -1,9 +1,5 @@
 package bots;
 
-import java.util.Iterator;
-import java.util.List;
-import java.util.ArrayList;
-
 /*
  *
  * Laat Bucket dingen vasthouden!
@@ -13,52 +9,10 @@ import java.util.ArrayList;
 public class HoudVast extends AnswerBot
 {
     
-    String items;
-    Boolean itemsWasLeeg;
-    
-    HoudVast(){
-        items = "";
-        itemsWasLeeg = true;
-    }
     
     @override
     public String ask(String in, String who)
     {
-        
-        String msg = "/me ";
-        if(in.toLowerCase().contains("geeft bucket "))
-        {
-            String item;
-            String[] words = in.split(" ");
-            if(words.length > 3)
-            {
-                item = words[3];
-                for(int i = 4; i < words.length && i < 20; i++)
-                {
-                    item += " ";
-                    item += words[i];
-                }
-            }
-            else return "Sorry, wat geef je me?";
-            
-            if(itemsWasLeeg)
-            {
-                msg += "heeft ";
-                msg += item;
-                items = item;
-                itemsWasLeeg = false;
-            }
-            else 
-            {
-                msg += "geeft ";
-                msg += (who + " ");
-                msg += (items + " ");
-                msg += "in ruil voor ";
-                msg += item;
-                items = item;
-            }
-
-            return (msg + ".");
-        } else{return void;}
+      return "Vasthouden!";
     }
 }
