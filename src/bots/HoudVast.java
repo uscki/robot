bd12748a2984f4cc5194d1396a5d72d3f14499d6
Rick <ikbenrick+git@gmail.com>
2012-10-24 15:02:10 +0200

    HoudVast nu eindelijk wel goed ingevuld

--- src/bots/HoudVast.java
+++ src/bots/HoudVast.java
@@ -1,5 +1,9 @@
 package bots;
 
+import java.util.Iterator;
+import java.util.List;
+import java.util.ArrayList;
+
 /*
  *
  * Laat Bucket dingen vasthouden!
@@ -8,11 +12,63 @@ package bots;
 
 public class HoudVast extends AnswerBot
 {
+    //De variabele die opslaat wat er allemaal in Bucket zit.
+    ArrayList<String> inhoud;
+    //De variabele die opslaat wat Bucket vasthoudt.
+    String item;
     
+    HoudVast()
+    {
+        inhoud = new ArrayList<String>();
+        item = "";
+    }
     
     @Override
     public String ask(String in, String who)
     {
-      return "Vasthouden!";
+        String msg;
+        
+        if(in.toLowerCase().contains("geeft bucket"))
+        {
+            String[] invoer;
+            invoer = in.split(" ");
+            
+            String temp;
+            
+            if(invoer[1] == "geeft")
+            {
+                temp = invoer[3];
+                for(int i = 4; i < invoer.length; i++)
+                {
+                    temp += " ";
+                    temp += invoer[i];
+                }
+                msg = ("/me geeft " + who + " een " + item + " in ruil voor " + temp);
+                item = temp;
+                return msg;
+            }
+        }
+        else if(in.toLowerCase().contains("stopt") && in.toLowerCase().contains("in bucket"))
+        {
+            String[] invoer;
+            invoer = in.split(" ");
+            
+            String temp;
+            
+            if(invoer[1] == "stopt")
+            {
+                temp = invoer[2];
+                for(int i = 3; i < invoer.length-2;i++)
+                {
+                    temp += " ";
+                    temp += invoer[i];
+                }
+                inhoud.add(temp);
+                
+                msg = "/me heeft nu " + temp + " in zich";
+                return msg;
+            }
+        }
+        return null;
     }
 }
