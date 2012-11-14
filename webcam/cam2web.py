cc282da3ce7bea48a7915c8d37fe2a8fdf4efb1a
Sjoerd Dost <deosjr@gmail.com>
2012-11-14 19:47:04 +0100

    Webcamscript in Python

---
+++ webcam/cam2web.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python2
+
+import cv2.cv as cv
+import time
+import os
+
+capture = cv.CreateCameraCapture(-1)
+
+cv.SetCaptureProperty(capture,cv.CV_CAP_PROP_FRAME_WIDTH, 640)
+cv.SetCaptureProperty(capture,cv.CV_CAP_PROP_FRAME_HEIGHT, 480);
+
+t_prev = time.clock()
+delay = 1.0
+
+while True:
+
+        #capture = cv.CreateCameraCapture(-1)
+        img = cv.QueryFrame(capture)
+
+        if img is not None:
+
+                cv.SaveImage("/home/mennov1/webcam/webcam.jpg",img)
+                os.system("curl -k -F \"uploadedfile=@/home/mennov1/webcam/webcam.jpg\" https://www.uscki.nl/~kruit/zebra/modules/Webcam/upload.php >> /dev/null")
+        else:
+                print "Something went wrong.."
+
+        t = time.clock()
+        while t < (t_prev + delay):
+                t = time.clock()
+        t_prev = time.clock()
+
+        #capture = None
