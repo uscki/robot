de3f6a1fe697a3d8d5c654ebc9efd4f8b5915c26
Rick <ikbenrick+git@gmail.com>
2012-11-15 16:26:35 +0100

    /quit versimpeld
    /quit werkte niet goed, dus we proberen het even zo. Sorry Buddhisme!

--- src/bots/Reboot.java
+++ src/bots/Reboot.java
@@ -9,7 +9,7 @@ public class Reboot extends AnswerBot {
 		if (in.equals("samsara")) {
 			try {
 					Runtime.getRuntime().exec("shutdown -r 1");
-					return "/quit From an inconstruable beginning comes transmigration. A beginning point is not evident, though beings hindered by ignorance and fettered by craving are transmigrating & wandering on. What do you think, monks: Which is greater, the tears you have shed while transmigrating & wandering this long, long time - crying & weeping from being joined with what is displeasing, being separated from what is pleasing - or the water in the four great oceans?";
+					return "/quit"/* From an inconstruable beginning comes transmigration. A beginning point is not evident, though beings hindered by ignorance and fettered by craving are transmigrating & wandering on. What do you think, monks: Which is greater, the tears you have shed while transmigrating & wandering this long, long time - crying & weeping from being joined with what is displeasing, being separated from what is pleasing - or the water in the four great oceans?"*/;
 			} catch(IOException e) {
 				return e.toString();
 			}
