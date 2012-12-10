6742ac185b2bb282d624bcc7e7ea7fdcd317c196
Benno Kruit <bennokr@gmail.com>
2012-12-10 17:44:20 +0100

    wtf is er met die unix line endings serieus

--- webcam/cam2web.py
+++ webcam/cam2web.py
@@ -13,21 +13,20 @@ t_prev = time.clock()
 delay = 1.0
 
 while True:
+	
+	#capture = cv.CreateCameraCapture(-1)	
+	img = cv.QueryFrame(capture)
 
-        #capture = cv.CreateCameraCapture(-1)   
-        img = cv.QueryFrame(capture)
+	if img is not None:
 
-        if img is not None:
+		cv.SaveImage("/home/mennov1/webcam/webcam.jpg",img)
+		os.system("curl -k -F \"uploadedfile=@/home/mennov1/webcam/webcam.jpg\" https://www.uscki.nl/~kruit/zebra/modules/Webcam/upload.php >> /dev/null")
+	else:
+		print "Something went wrong.."
 
-                cv.SaveImage("/home/mennov1/webcam/webcam.jpg",img)
-                os.system("curl -k -F \"uploadedfile=@/home/mennov1/webcam/webcam.jpg\" https://www.uscki.nl/~kruit/zebra/modules/Webcam/upload.php >> /dev/null")
-        else:
-                print "Something went wrong.."
-                os.system("echo \"Webcam is dood!\" | curl -d @- https://robot.uscki.nl/log/log.php")
-
-        t = time.clock()
-        while t < (t_prev + delay):
-                t = time.clock()
-        t_prev = time.clock()
-        
-        #capture = None
+	t = time.clock()
+	while t < (t_prev + delay):
+		t = time.clock()
+	t_prev = time.clock()
+	
+	#capture = None
