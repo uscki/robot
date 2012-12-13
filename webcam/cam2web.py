b20dc1ee413bc8c521baa26b6c909a4645fce8cd
Benno Kruit <bennokr@gmail.com>
2012-12-13 15:39:38 +0100

    Meer webcam-debugdingen

--- webcam/cam2web.py
+++ webcam/cam2web.py
@@ -14,19 +14,18 @@ delay = 1.0
 
 while True:
 	
-	#capture = cv.CreateCameraCapture(-1)	
-	img = cv.QueryFrame(capture)
-
-	if img is not None:
-
-		cv.SaveImage("/home/mennov1/webcam/webcam.jpg",img)
-		os.system("curl -k -F \"uploadedfile=@/home/mennov1/webcam/webcam.jpg\" https://robot.uscki.nl/webcam.php")
+	if capture is not None:
+		img = cv.QueryFrame(capture)
+		if img is not None:
+			cv.SaveImage("/home/mennov1/webcam/webcam.jpg",img)
+			os.system("curl -k -F \"uploadedfile=@/home/mennov1/webcam/webcam.jpg\" https://robot.uscki.nl/webcam.php")
+		else:
+			print "No image"
 	else:
-		print "Something went wrong.."
+		print "No capture, trying to re-init"
+		capture = cv.CreateCameraCapture(-1)
 
 	t = time.clock()
 	while t < (t_prev + delay):
 		t = time.clock()
 	t_prev = time.clock()
-	
-	#capture = None
