8fd705daa11ecabab9bb1178c89f352da63e0bf4
Benno Kruit <bennokr@gmail.com>
2013-01-22 18:29:46 +0100

    WebcamClient logging

--- src/mennov1/WebcamClient.java
+++ src/mennov1/WebcamClient.java
@@ -1,13 +1,14 @@
 package mennov1;
 
 import static com.googlecode.javacv.cpp.opencv_highgui.cvSaveImage;
+import lib.SewerSender;
 
 import com.googlecode.javacv.FrameGrabber;
 import com.googlecode.javacv.OpenCVFrameGrabber;
 import com.googlecode.javacv.cpp.opencv_core.IplImage;
 
 public class WebcamClient implements Runnable {
-    final int INTERVAL=1000;///you may use interval
+    final int INTERVAL=5000;///you may use interval
     IplImage image;
     @Override
     public void run() {
@@ -15,20 +16,20 @@ public class WebcamClient implements Runnable {
         try {
             grabber.start();
             IplImage img;
+            SewerSender.logMessage("Started webcam");
             while (true) {
                 img = grabber.grab();
                 if (img != null) {
                     cvSaveImage("webcam/capture.jpg", img);
-                    System.out.println("Grab");
                     //Just for benno
                     try {
                     	Runtime.getRuntime().exec("webcam/upload");
                     } catch (Exception e) { }
                 } else {
-                	System.out.println("Nongrab");
+                	System.out.println("Webcam frame is null");
                 }
                 Thread.sleep(INTERVAL);
             }
-        } catch (Exception e) { System.out.println("webcam kapot huilen!"); }
+        } catch (Exception e) { SewerSender.println("webcam kapot huilen!"); }
     }
 }
