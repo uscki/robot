f7fcf98d9789b759a2c176b9e55709cc69bf08d6
Benno Kruit <bennokr@gmail.com>
2013-04-24 21:44:12 +0200

    Snorbot met lokaal plaatje opslaan

--- src/mennov1/ThuisTester.java
+++ src/mennov1/ThuisTester.java
@@ -2,6 +2,8 @@ package mennov1;
 
 import java.io.IOException;
 
+import bots.FaceSaver;
+
 /**
  * 
  * The home testing class for the robot. This initializes a subset of the communication channels (clients) to the bothandler.
@@ -22,20 +24,20 @@ public class ThuisTester {
 		// Gebruik de ImageFileClient om een eigen webcam te simuleren
 //		bus.addListener(new ImageFileClient("/home/mennov1/webcam/webcam.jpg", 1000L));
 		// Of start een echte webcam-client die OpenCV gebruikt
-//		try {
-//			Thread t = new Thread(new WebcamClient());
-//			t.start();
-//		} catch (Exception e) {}
+		try {
+			Thread t = new Thread(new WebcamClient());
+			t.start();
+		} catch (Exception e) {}
 
 		// Als het tweede argument null is, slaat hij het plaatje op zonder te uploaden
 //		bus.addListener(new ImageUploader("webcam/capture.jpg", null));
 		
 		// Sla een afbeelding op met rechthoeken om de gezichten
-//		bus.addListener(new FaceSaver("webcam/faces.jpg"));
+		// bus.addListener(new FaceSaver("webcam/faces.jpg"));
 		
 		
 		// Laat zien dat je leeft op robot.uscki.nl
-//		System.out.println(SewerSender.sentLifeSign()? "Sent life sign" : "Life sign failed");
+		// System.out.println(SewerSender.sentLifeSign()? "Sent life sign" : "Life sign failed");
 
 		try {
 			System.in.read();
