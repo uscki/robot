3b8d9dbb0028755b0e1bbb71c7a9e6fe2d7d1e54
Benno Kruit <bennokr@gmail.com>
2013-06-19 23:21:30 +0200

    FacebookChat test

---
+++ test/fb/CustomSASLDigestMD5Mechanism.java
@@ -0,0 +1,45 @@
+package fb;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.security.auth.callback.CallbackHandler;
+import javax.security.sasl.Sasl;
+
+import org.jivesoftware.smack.SASLAuthentication;
+import org.jivesoftware.smack.XMPPException;
+import org.jivesoftware.smack.sasl.SASLMechanism;
+
+public class CustomSASLDigestMD5Mechanism extends SASLMechanism {
+
+	public CustomSASLDigestMD5Mechanism(SASLAuthentication saslAuthentication) {
+		super(saslAuthentication);
+	}
+
+	@Override
+	public void authenticate(String username, String host, String password)
+			throws IOException, XMPPException {
+		this.authenticationId = username;
+		this.password = password;
+		this.hostname = host;
+
+		String[] mechanisms = { getName() };
+		Map<String, String> props = new HashMap<String, String>();
+		sc = Sasl.createSaslClient(mechanisms, null, "xmpp", host, props, this);
+		super.authenticate();
+	}
+
+	@Override
+	public void authenticate(String username, String host, CallbackHandler cbh)
+			throws IOException, XMPPException {
+		String[] mechanisms = { getName() };
+		Map<String, String> props = new HashMap<String, String>();
+		sc = Sasl.createSaslClient(mechanisms, null, "xmpp", host, props, cbh);
+		super.authenticate();
+	}
+
+	protected String getName() {
+		return "DIGEST-MD5";
+	}
+}
