d61e67dbabad95a7038c10c7d18f47611a446219
Benno Kruit <bennokr@gmail.com>
2014-06-16 16:01:27 +0200

    javacv is een poepje

--- src/mennov1/MennoV1.java
+++ src/mennov1/MennoV1.java
@@ -16,19 +16,20 @@ public class MennoV1 {
 
 		EventBus bus = EventBus.getInstance();
 		bus.addListener(BotHandler.getInstance());
-		bus.addListener(FacebookClient.getInstance());
+//		bus.addListener(FacebookClient.getInstance());
 		bus.addListener(JabberClient.getInstance());
-		bus.addListener(IrcClient.getInstance());
+//		bus.addListener(IrcClient.getInstance());
 		bus.addListener(TerminalClient.getInstance());
+		System.out.println(System.getProperty("java.library.path"));
 
-		try {
-			Thread t = new Thread(new WebcamClient());
-			t.start();
-		} catch (Exception e) {
-			SewerSender.println("webcam kapot huilen!\n"+e.toString());
-		}
+//		try {
+//			Thread t = new Thread(new WebcamClient());
+//			t.start();
+//		} catch (Exception e) {
+//			SewerSender.println("webcam kapot huilen!\n"+e.toString());
+//		}
 
-		bus.addListener(new ImageUploader("https://robot.uscki.nl/webcam.php"));
+//		bus.addListener(new ImageUploader("https://robot.uscki.nl/webcam.php"));
 
 		// Send a sign of life to robot.uscki.nl
 		System.out.println(SewerSender.sentLifeSign()? "Sent life sign" : "Life sign failed");
